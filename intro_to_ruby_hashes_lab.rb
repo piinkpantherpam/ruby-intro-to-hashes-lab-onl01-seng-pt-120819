dc56e32d4f7b68809a7ccdcef5dc30bda9a56073
Pam Cairo <pcairo_90@outlook.com>
2019-12-23 01:13:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,10 +17,10 @@ def monopoly_with_second_tier
 	    monopoly = {
 	
 	            :railroads=>
- 		{
- 		:pieces=>4,
- 		:names=>{},
- 		:rent_in_dollars=>{}
+ 		          {
+ 		          :pieces=>4,
+ 		          :names=>{},
+ 		          :rent_in_dollars=>{}
  		}
  }
 
